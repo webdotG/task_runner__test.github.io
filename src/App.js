09905e03feaa0d11ebe5a98999700483075b9b57
webDotG <webdotg.inbox@gmail.com>
2023-04-22 15:08:21 +0300

    add function delet item tasklist

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Header from './components/Header/Header';
+import AddToDo from './components/AddToDo/AddToDo';
+import ToDoList from './components/ToDoList/ToDoList';
+
 
 function App() {
+    //useState хранит в себе всю информацию, аналог базы данных
+    //вся информация массива с обьектами доступна в переменной TODO
+    //SETTODO функция которая будет менять работать с TODO с массивом обьектов
+const [todo, setTodo] = useState([
+    {
+      id: 1,
+      title: 'first task',
+      status: true,
+
+    },
+    {
+      id: 2,
+      title: 'second task',
+      status: true,
+
+    },
+    {
+      id: 3,
+      title: 'third task',
+      status: false,
+
+    },
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <AddToDo/>
+      <ToDoList todo={todo} setTodo={setTodo} />
     </div>
   );
 }
 
+
+//передаю компоненту todolist PROPS пропсы для того что бы можно было работать с этим компонентом
+//говорю что todo равно массиву обьектов todo и settodo равно фуекцик settodo
+     
 export default App;
