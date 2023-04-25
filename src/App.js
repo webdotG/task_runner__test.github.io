12c67287af9b49b146c0d79afda40c3ad2090dbc
webDotG <webdotg.inbox@gmail.com>
2023-04-25 18:28:35 +0300

    chto-to rabotaet uge ne ploho

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
     handleSubmit,
     reset,                                  //метод для очистки формы после отправки
   } = useForm({                             //в хук передаю обьект с настройками проверки
-      mode: 'onChange'                      //mode-режим выбрал при изменении данных
+      mode: 'onBlur'                      //mode-режим выбрал при изменении данных
   });                       
 
 //кастомный способ обработки формы
@@ -32,11 +32,6 @@ function App() {
     alert(JSON.stringify(data));
     reset();                                //очищаю форму после отправки
   }
-
-
-
-
-
 
 //useState хранит в себе всю информацию, аналог базы данных
   //вся информация массива с обьектами доступна в переменной TODO
@@ -51,15 +46,16 @@ function App() {
   ,[todo])
   
 //todo.forEach((i) => console.log(i.id))
-  
-  return (
-    <div className={style['App']}>
-
 
 //сначла буду вызывать метод handlesubmit который идкт из коробки хука useform 
   //уже в нём буду использовать свой кастомный хэндлер onsubmit рписаный выше  
+
+  return (
+    <div className={style['App']}>
+
   <form onSubmit={handleSubmit(onSubmit)} className={['form-login']}>
     <div className={['form-login__name-wrapper']}> 
+      <label class={['form-login__name-label']}>log in</label>
       <input                                             //'firstname'=name'firstname' уникальный ключ
         {...register('firstname', {                      //метод импортированный из коробки хука useform-вохвращает обьект и для работы использую деструктуризацию
           required: 'поле обязательно к заполнению',     //задаю правила для input вывожу сообщение если requirde не заполнен
@@ -70,22 +66,13 @@ function App() {
         })}        
       />
       <div style={{height: 40}}>
-        {errors?.firstname && <p>{}errors?.firstname?.message || 'ошибка firstname'</p>}
+        {errors?.firstname && <p>{errors?.firstname?.message || 'ошибка firstname'}</p>}
       </div>
     </div>
-    <input                                             
-      {...register('lasttname', {                      
-        required: 'поле обязательно к заполнению',     
-        minLength: {
-          value: 5,                                    
-          message: 'миниимальное количество символов 5' 
-        }
-      })}        
-    />
-    <div style={{height: 40}}>
-      {errors?.lastname && <p>{}errors?.lastname?.message || 'ошибка lastname'</p>}
-    </div>
     
+
+
+
     <input type='submit'/>
   </form>
 
