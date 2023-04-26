6932faabc95b0e0a7752833d6f8b1f933ac2c5e0
webDotG <webdotg.inbox@gmail.com>
2023-04-26 10:32:23 +0300

    login form style err and base style add

--- src/components/FormLogin/FormLogin.jsx
+++ src/components/FormLogin/FormLogin.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import style from './FormLogin.module.css';
+import { type } from '@testing-library/user-event/dist/type';
 
 //сначла буду вызывать метод handlesubmit который идкт из коробки хука useform 
 //уже в нём буду использовать свой кастомный хэндлер onsubmit рписаный выше  
@@ -11,56 +12,68 @@ function FormLogin() {
   //метод register для регистрации полей формы
   //formstate обьект со свойствами я использую свойство errors для вывода ошибок
   //handleSubmit функция которая будет вызываться при нажатии на кнопку submit
-  const  {
+  const {
     register,
     formState: { errors, isValid },
     handleSubmit,
     reset,                          //метод для очистки формы после отправки
   } = useForm({                     //в хук передаю обьект с настройками проверки
-    mode: 'onBlur'                  //mode-режим выбрал при изменении данных
+    mode: 'onChange'                  //mode-режим выбрал при изменении данных
   })
-  
 
-//кастомный способ обработки формы
+
+  //кастомный способ обработки формы
   //дефолтный handlesubmit будет передавать данные полей формы в мою data
   //если в handlesubmit есть какие-то ошибки то кастомный onsubmit вызываться не будет
   //onsubmit будет срабатывать тольео если нет ошибок
   //для проверки создаю алерт что бы сотреть что вообще передаётся
-const onSubmit = (data) => {
-  alert(JSON.stringify(data));
-  reset();                         //очищаю форму после отправки
-}
+  const onSubmit = (data) => {
+    alert(JSON.stringify(data));
+    reset();                         //очищаю форму после отправки
+  }
 
   return (
-  <form onSubmit={handleSubmit(onSubmit)} className={style['form-login']}>
+    <form onSubmit={handleSubmit(onSubmit)} className={style['form-login']} autoComplete='off'>
       <div className={style['form-login__name-wrapper']}>
         <label class={style['form-login__name-label']}>log in</label>
         <input className={style['form-login__name-input']}                                             //'firstname'=name'firstname' уникальный ключ
-          {...register('firstname', {                      //метод импортированный из коробки хука useform-вохвращает обьект и для работы использую деструктуризацию
+          {...register('user', {                      //метод импортированный из коробки хука useform-вохвращает обьект и для работы использую деструктуризацию
             required: 'обязательно заполнить',             //задаю правила для input вывожу сообщение если requirde не заполнен
             minLength: {
               value: 5,                                    //задаю минимальное еоличество символов 5
               message: 'минимум 5 символов'                //передаю сообщение об ошибке 
             }
-          })}
-        />
-        <div style={{ height: 20 }}>
-          {errors?.firstname && <p>{errors?.firstname?.message || 'ошибка firstname'}</p>}
+          })} />
+        <div className={style['form-login__name-error']}>
+         {errors?.user && <p>{errors?.user?.message || 'минимум 5 символов'}</p>}
         </div>
       </div>
       <div className={style['form-login__password-wrapper']}>
-          <label className={style['form-login__password-label']} for="form-login-password">password</label>
-          <input className={style['form-login__password-input']} id="form-login-password" name="form-login-password" type="password" placeholder="password"></input>
+        <label className={style['form-login__password-label']} for="form-login-password">password</label>
+        <input className={style['form-login__password-input']} type='password' 
+          {...register('password', {                      //метод импортированный из коробки хука useform-вохвращает обьект и для работы использую деструктуризацию
+            required: 'обязательно заполнить',             //задаю правила для input вывожу сообщение если requirde не заполнен
+            minLength: {
+              value: 5,                                    //задаю минимальное еоличество символов 5
+              message: 'минимум 5 символов'                //передаю сообщение об ошибке 
+            },
+          })} 
+        />
+        <div className={style['form-login__password-error']}>
+         {errors?.password && <p>{errors?.password?.message || 'неверный ввод'}</p>}
+        </div>
       </div>
       <div className={style['form-login__remember-wrapper']}>
-          <label className={style['form-login__remember-label']} for="form-login-remember">remeber me</label>
+        <div className={style['form-login__remember-input-label__wrapper']}>
           <input className={style['form-login__remember-input']} id="form-login-remember" type="checkbox"></input>
+          <label className={style['form-login__remember-label']} for="form-login-remember">запомнить</label>
+        </div>
+        <a className={style['form-login__forgot-password-link']}>забыли пароль ?</a>
       </div>
       <div className={style['form-login__forgot-password__wrapper']}>
-          <a className={style['form-login__forgot-password']}>forgot password ?</a>
-      </div>
+              </div>
       <div className={style['form-login__submit-btn__wrapper']}>
-          <button className={style['form-login__submit-btn']} type="submit" disabled={!isValid}>enter</button>
+        <button className={style['form-login__submit-btn']} type="submit" disabled={!isValid}>enter</button>
       </div>
     </form>
   )
