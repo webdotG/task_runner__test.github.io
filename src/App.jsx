92e775d9d29f1ee358236acf3e4fc4745663b01c
webDotG <webdotg.inbox@gmail.com>
2023-04-28 15:48:16 +0300

    rolled back before using BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { Routes, Route, } from "react-router-dom";
 import style from "../src/App.css";
-import { Layout } from './components/Layout/Layout'
+// import { Layout } from './components/Layout/Layout'
+// import { Routes, Route, } from "react-router-dom";
 
 //импортирую страницы
 import FormLogin from "./components/FormLogin/FormLogin";
+import Header from './components/Header/Header'
 import AddToDo from "./components/AddToDo/AddToDo";
 import ToDoList from "./components/ToDoList/ToDoList";
 
@@ -23,21 +24,23 @@ function App() {
 
 	return (
 		<div className={style["App"]}>
-			<Routes>
-				 <Route path="/" element={ <Layout/> }>  {/*создаю route в который кладу дочерние элементы */}
-					<Route index element={<FormLogin />} />{/*использую ключ index что бы показать главную страницу */}
-					<Route path="add" element={<AddToDo todo={todo} setTodo={setTodo} />}/>{/**/}
-					<Route path="list" element={<ToDoList todo={todo} setTodo={setTodo} />}/>
-				</Route>
-			</Routes>
+			<FormLogin />
+			<Header />
+			<AddToDo todo={todo} setTodo={setTodo} />
+			<ToDoList todo={todo} setTodo={setTodo} />
+
 		</div>
 	);
 }
 
-// <FormLogin />
-// <Header />
-// <AddToDo todo={todo} setTodo={setTodo} />
-// <ToDoList todo={todo} setTodo={setTodo} />
+
+// <Routes>
+// 				 <Route path="/" element={ <Layout/> }>  {/*создаю route в который кладу дочерние элементы */}
+// 					<Route index element={<FormLogin />} />{/*использую ключ index что бы показать главную страницу */}
+// 					<Route path="add" element={<AddToDo todo={todo} setTodo={setTodo} />}/>{/**/}
+// 					<Route path="list" element={<ToDoList todo={todo} setTodo={setTodo} />}/>
+// 				</Route>
+// 			</Routes>
 
 //передаю компоненту todolist PROPS пропсы для того что бы можно было работать с этим компонентом
 //говорю что todo равно массиву обьектов todo и settodo равно фуекцик settodo
